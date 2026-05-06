967a3c7a8f1a6075e9b770fb13132bec2adeda48
minsung-k <minsung.kang94@gmail.com>
2026-05-06 12:23:06 -0400

    Delete foo-template directory

--- foo-template/src/foo/__init__.py
+++
@@ -1,3 +0,0 @@
-"""foo: short description of project."""
-
-__version__ = "0.0.1"
--- foo-template/src/foo/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from foo.utils.run_dir import stamp_run_dir
-from foo.utils.seeding import set_seed
-
-__all__ = ["set_seed", "stamp_run_dir"]
